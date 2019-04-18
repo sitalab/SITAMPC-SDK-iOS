eb854305b655bcfac3dee4af91c93dbfc677032f
zanzoon <zeinab@itx.com.lb>
2019-04-18 12:21:38 +0300

    Added missing asset

--- Example/SITAMPC/MPCAppDelegate.m
+++ Example/SITAMPC/MPCAppDelegate.m
@@ -16,7 +16,7 @@
     //    [MPCFlowManager sharedManager].MPC_host = <INSERT MPC_host>;
     //    [MPCFlowManager sharedManager].MPC_apiKey = <INSERT MPC_apiKey>;
     //    [MPCFlowManager sharedManager].MPC_cid = <INSERT MPC_cid>;
- 
+    
     [MPCFlowManager sharedManager].delegate = self;
     [[MPCFlowManager sharedManager]flowSetup];
     [[MPCFlowManager sharedManager] showIntroScreensOnRun:false];
